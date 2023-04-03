a12b51bd347dc206f18ccdcb4e72ed406b1bcbf1
TryTwoTop <trytwotop@gmail.com>
2023-04-04 01:24:52 +0900

    [진동민] C언어 인터페이스 실습 파일 업로드

---
+++ dongmin/4.4/interface/protocol.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include "protocol.h"
+
+void process_power (void)
+{
+    printf("Processing Power\n");
+}
+
+void process_channel_up (void)
+{
+    printf("Processing Channel Up\n");
+}
+
+void process_channel_down (void)
+{
+    printf("Processing Channel Down\n");
+}
+
+void process_volume_up (void)
+{
+    printf("Processing Volume Up\n");
+}
+
+void process_volume_down (void)
+{
+    printf("Processing Volume Down\n");
+}
+
+void process_play (void)
+{
+    printf("Processing Play\n");
+}
+
+void process_pause (void)
+{
+    printf("Processing Pause\n");
+}
